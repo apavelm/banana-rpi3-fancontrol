a5e8b4b8b06ea993f35c9c9443198e76c73334d5
Pavel Andreev <apavelm@users.noreply.github.com>
2026-05-03 20:22:02 +0400

    Clean up fan_metrics.sh by removing commented code
    Removed unused exec and commented out printf statements.

--- fan_metrics.sh
+++ fan_metrics.sh
@@ -7,8 +7,6 @@ TEMP_STATE_FILE="/var/run/fancontrol_temp"
 
 INTERVAL=10
 
-#exec 3<> >(socat - UNIX-CONNECT:$SOCK)
-
 while true; do
   # PWM
   if [ -f "$PWM_STATE_FILE" ]; then
@@ -34,13 +32,6 @@ while true; do
 
   echo "PUTVAL \"$HOST/fan/fanpwm\" $TS:$PWM_INV"
   echo "PUTVAL \"$HOST/fan/fantemp\" $TS:$TEMP_FLOAT"
-
-
-  # PWM (int)
-  #printf "PUTVAL \"%s/fan/fanpwm\" %d:%d\n" "$HOST" "$TS" "$PWM_INV" >&3
-
-  # TEMP (float)
-  #printf "PUTVAL \"%s/fan/fantemp\" %d:%s\n" "$HOST" "$TS" "$TEMP_FLOAT" >&3
 
   sleep "$INTERVAL"
 done
